ed377b02bb27552b7ca4247a42e2ba2a3d527dab
DSB <3318977327@qq.com>
2026-08-18 01:43:07 +0800

    恢复app_config.c BOM(编码铁律) + 灵敏度默认值5改3 + 日志补8-18晚间设计(时间RTC+软件定时器方案、K0设备开关方案)

--- App/Src/app_config.c
+++ App/Src/app_config.c
@@ -5,4 +5,4 @@
 #include "app_config.h"
 
 /* 默认配置：光标大小1，灵敏度5（阶段3后从 Flash 加载覆盖） */
-sys_config_t g_sys_cfg = { .cursor_size = 1, .cursor_sens = 5 };
+sys_config_t g_sys_cfg = { .cursor_size = 1, .cursor_sens = 3 };
